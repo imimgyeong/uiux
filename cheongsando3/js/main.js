e9400b837961281f4548f5530fcc2a624769b2b9
imimgyeong <imimgyeong2@gmail.com>
2025-09-11 11:31:47 +0900

    250911 최종뷰포트삭제

---
+++ cheongsando3/js/main.js
@@ -0,0 +1,36 @@
+
+$(document).ready(function(){
+    //console.log('연결')
+    /*
+        .tour .list ul li에 마우스를 올리면 마우스를 올린 li에만 on클래스를 추가해야함
+        -->마우스를 오버하면 모든 li에 있는 on 클래스를 지움(없는 애는 가만히있고 있는애만 지움)
+        누가 on클래스를 가지고 있는지 모르니까
+    */
+   $('.tour .list ul li').on('mouseenter', function(){
+    //console.log('오버했다!')
+    $('.tour .list ul li').removeClass('on')
+    $(this).addClass('on')
+   })
+   
+   $('footer .right_area .family_site button.family_open').on('click', function(){
+        // console.log('여는 버튼 클릭')
+        $('footer .right_area .family_site').addClass('open')
+    })
+    $('footer .right_area .family_site button.family_close').on('click', function(){
+        // console.log('닫는 버튼 클릭')
+        $('footer .right_area .family_site').removeClass('open')
+    })
+
+    /*footer .right_area .top를 클릭하면 브라우저가 상단으로 스크롤이 됨*/
+    $('footer .right_area .top').on('click', function(){
+        console.log('누름')
+        let scrolling = $(window).scrollTop()
+        console.log(scrolling)
+        // $(window).scrollTop(100)
+        $('html, body').animate({
+            scrollTop : 0
+        }, 500)
+    })
+
+})//$(document).ready 무조건 맨 아래
+
