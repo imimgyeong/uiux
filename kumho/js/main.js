19906c19e702334a9896b62958238995f8b1d0b6
imimgyeong <imimgyeong2@gmail.com>
2025-10-27 18:00:49 +0900

    251027 금호건설

--- kumho/js/main.js
+++ kumho/js/main.js
@@ -16,9 +16,59 @@ $(document).ready(function(){
         infinite: true, //무한반복 (loop)
     });
 
+/* -------------------------------------------
+ * .biz .list ul li에 마우스를 오버하면
+    - 오버한 li에 active 클래스 추가
+    - .biz .list에는 over 클래스 추가
+    - 언제 out?
+---------------------------------------------*/
+    $('.biz .list ul li').on('mouseenter', function(){
+        $(this).addClass('active')
+        $('.biz .list').addClass('over')
+    })
+    $('.biz .list ul li').on('mouseleave', function(){
+        $(this).removeClass('active')
+        $('.biz .list').removeClass('over')
+    })
 
+    const news_swiper = new Swiper('.news .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            769: {    /* 640px 이상일때 적용 */
+                slidesPerView: 2,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 24,
+            },
+            1025: {    /* 640px 이상일때 적용 */
+                slidesPerView: 3,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 40,
+            },
+        },
+        scrollbar: {
+            el: ".news .ctrl_wrap .scrollbar",
+            hide: false,
+            draggable: true, // 스크롤바 드래그 기능
+            dragSize: 200, // 스크롤바 사이즈
+          },
+       
+    });
 
+    /* --------------------------------------------------
+        *.service .list ul li에 마우스를 오버했을때
+         -오버한 li에 있는 data-bg의 값을 
+         .service .list한테 class명으로 줌 
+    -----------------------------------------------------*/
 
+    let service_name
+    $('.service .list ul li').on('mouseenter', function(){
+        service_name = $(this).attr('data-bg')
+        // console.log(service_name)
+        $('.service .list').attr('data-bg', service_name)
+        //.attr 기존에 있던 값은 지우고 내가 준 값으로 새로 채우는것 
+    })
+    $('.service .list').on('mouseleave', function(){
+        $('.service .list').attr('data-bg', ' ')
+    })
 
 
 })//맨끝임 나가지마셈여
