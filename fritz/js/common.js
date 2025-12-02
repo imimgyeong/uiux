b8114c062f54ca4eb528f07efcbe1a38f5b10274
imimgyeong <imimgyeong2@gmail.com>
2025-12-02 18:03:43 +0900

    251202 프릳츠커피 시그니처

--- fritz/js/common.js
+++ fritz/js/common.js
@@ -1,129 +1,116 @@
+/*------------------------------------------------------------------
+ * 파일명 : common.js
+    - 작성자 : 황난경
+    - 작성일 : 2025-12-02
+    - 설  명 : header/footer만 적용되는 js를 저장
+---------------------------------------------------------------------*/
 $(document).ready(function(){
-    let mobile_size = 1024 //모바일 메뉴 시작 사이즈
-    let header_mobile_size = 768;  // header 메뉴만 모바일 시작
-    let window_w //브라우저 넓이
-    let device_status //현재 pc인지 mobile인지 구분하는 값
-    let header_device_status; // header device
+    let mobile_size = 1024
+    let window_w 
+    let device_status // pc, mobile
 
-    function device_chk(){
+    function device_chk(){ //함수를 정의한다(선언한다)
         window_w = $(window).width()
-        if(window_w > mobile_size){
+        if(window_w > mobile_size){ // 브라우저 넓이가 1024보다 클때 pc버전이라는말
             device_status = 'pc'
         }else{
             device_status = 'mobile'
         }
-        // header 전용 기준
-        header_device_status = (window_w > header_mobile_size) ? 'pc' : 'mobile'
-        // console.log(device_status)
+        console.log(device_status)
     }
-    device_chk() //문서가 로딩되었을때 1번실행
-    $(window).resize(function(){
-        device_chk() //브라우저가 리사이즈 할때마다 1번씩 실행
+
+    device_chk() // html의 로딩이 완료도니 이후 단 1번 실행
+    $(window).resize(function(){ // 브라우저가 리사이즈 될때마다 실행
+        device_chk()
     })
 
-    /*----------- 시작 : pc버전 메뉴오버 ----------*/
+    /*----------- 시작 : pc버전 메뉴오버 -----------*/
     $('header .gnb .gnb_wrap ul.depth1 > li').on('mouseenter focusin', function(){
-        if( device_status == 'pc'){
+        if(device_status == 'pc'){ //pc일때만 동작 
+            // console.log('오버했음')
             $('header').addClass('menu_pc')
             $('header .gnb .gnb_wrap ul.depth1 > li').removeClass('over')
             $('header .gnb .gnb_wrap ul.depth1 > li > ul.depth2').slideUp()
             $(this).addClass('over')
             $(this).find('.depth2').slideDown()
-            
         }
     })
-    $('header .gnb .gnb_wrap ul.depth1 > li').on('mouseleave ', function(){
+    $('header .gnb .gnb_wrap ul.depth1 > li').on('mouseleave', function(){
         $(this).removeClass('over')
         $(this).find('.depth2').slideUp()
     })
     $('header').on('mouseleave', function(){
         $(this).removeClass('menu_pc')
     })
+
     $('header .util .search .sch_open').on('foucusin', function(){
         $('header .gnb .gnb_wrap ul.depth1 > li').removeClass('over')
     })
-     /*----------- 끝 : pc버전 메뉴오버 -----------*/
+    /*----------- 끝 : pc버전 메뉴오버 -----------*/
 
-    /*----------- 시작 : 모바일버전 메뉴오버 (수정됨) -----------*/
-
-    let gnb_open
-
-    // 1. DEPTH1 메뉴 아코디언 로직 (클릭 위임 방식으로 변경)
-    // 'header' 내부에서 발생하는 클릭 이벤트를 감지하고 ul.depth1 > li > a 인 경우에만 실행
-    $('header').on('click', '.gnb .gnb_wrap ul.depth1 > li > a', function(e){
-        console.log(header_device_status);
-        if( header_device_status == 'mobile'){ // device_status 변수가 정의되어 있다고 가정합니다.
-            
-            // 닫기 버튼이 아닌, 메뉴의 depth1을 클릭했을 때만 e.preventDefault() 실행
-            // (gnb_open 버튼 클릭 시는 작동하면 안 됨)
+    /*----------- 시작 : 모바일버전 1차메뉴 클릭 -----------
+    * 닫혀있는 메뉴를 클릭하면 기존에 열려있던 다른메뉴를 닫고 나만열기(open클래스 추가)
+    * 열려있는 메뉴를 클릭하면 나 자신을 닫고 끝남
+    * 열린메뉴, 닫힌메뉴를 구분하는 방법 - open있으면 열린메뉴, 없으면 닫힌메뉴 
+    * 1차메뉴 a의 링크를 삭제 (링크 이동을 못하게 막아야함 ?2차메뉴가 열려야하니까)
+    */
+    $('header .gnb .gnb_wrap ul.depth1 > li > a').on('click', function(e){
+        if(device_status == 'mobile'){
             e.preventDefault();
-            
-            gnb_open = $(this).parent().hasClass('open');
-            
-            if(gnb_open == true){ // 열려있다면 닫기
-                $(this).parent().removeClass('open');
-                $(this).next().slideUp();
-            }else{ // 닫혀있다면 열기
-                $('header .gnb .gnb_wrap ul.depth1 > li').removeClass('open');
-                $('header .gnb .gnb_wrap ul.depth1 > li > ul.depth2').slideUp();
-                $(this).parent().addClass('open');
-                $(this).next().slideDown();
+            if($(this).parent().hasClass('open') == true){ //열려있는 메뉴를 다시 클릭했을때
+                // console.log('열림')
+                $(this).parent().removeClass('open')
+                $(this).next().slideUp() //2차메뉴를 슬라이드로 닫기
+            }else{//닫혀있는 다른 메뉴를 다시 여는것
+                // console.log('닫힘')
+                $('header .gnb .gnb_wrap ul.depth1 > li').removeClass('open') /* 모든 li의 open을 삭제 */
+                $(' header .gnb .gnb_wrap ul.depth1 > li > ul.depth2').slideUp() //모든 2차메뉴 닫기
+                $(this).parent().addClass('open')
+                $(this).next().slideDown() //2차메뉴를 슬라이드로 열기
             }
         }
-    });
-
-
-    // 2. 메뉴 열기 버튼 클릭 이벤트 (클릭 위임 방식으로 변경)
-    // .gnb_open을 클릭하면 header에 .menu_mo 클래스를 추가
-    $('header').on('click', '.gnb_open', function(){
-        $('header').addClass('menu_mo');
-    });
-
-
-    // 3. 메뉴 닫기 버튼 클릭 이벤트 (작동하지 않던 핵심 문제 해결 및 초기화 로직 추가)
-    // .gnb_close를 클릭하면 header에서 .menu_mo 클래스를 제거
-    $('header').on('click', '.gnb_close', function(){
         
-        // 1. 메인 메뉴 닫기 (문제 해결)
-        $('header').removeClass('menu_mo');
-        
-        // 2. 하위 메뉴 (depth2) 정리 (추가 개선)
-        // 다음에 메뉴를 열었을 때 모든 depth2가 닫혀 있도록 초기화
-        $('header .gnb .gnb_wrap ul.depth1 > li').removeClass('open');
-        $('header .gnb .gnb_wrap ul.depth1 > li > ul.depth2').slideUp(); 
-    });
+    })
+    /*----------- 끝 : 모바일버전 1차메뉴 클릭 -----------*/
 
-    /*----------- 끝 : 모바일버전 메뉴오버 (수정됨) -----------*/
+    /*----------- 시작 : 모바일버전 2차메뉴 열기 -----------
+    * 열기를 클릭하면 header에 menu_mo 추가 (header .gnb .gnb_open)
+    * 닫기를 클릭하면 header에 menu_mo 삭제 (header .gnb .gnb_wrap .gnb_close)
+    */
+    $('header .gnb .gnb_open').on('click', function(){
+        $('header').addClass('menu_mo')
+    })
+    $('header .gnb .gnb_wrap .gnb_close').on('click', function(){
+        $('header').removeClass('menu_mo')
+    })
+    /*----------- 끝 : 모바일버전 2차메뉴 닫기 -----------*/
 
-     /*----------- 시작 :스크롤 시 header에 fixed -----------*/
-    let scrolling = $(window).scrollTop()//현재 스크롤된 값
-    let prev_scroll //이전에 스크롤된 값
-    let diff_scroll //차이 값(diffrent)
+    /*----------- 시작 : 스크롤 시 header에 fixed -----------
+    * pc mobile 둘다
+    * 스크롤이 조금만 되도 header에 fixed 클래스를 줌
+    * 다시 맨 꼭대기로 올라가면 header에 fixed 클래스 삭제
+    */
+
+    let scrolling //스크롤된 값
+
     function scroll_chk(){
-        prev_scroll = scrolling
-        scrolling = $(window).scrollTop()
-        diff_scroll = prev_scroll - scrolling 
-        // console.log(diff_scroll)
-        if(diff_scroll < 0){// 스크롤을 위로 올라간다는 뜻
-            $('header').addClass('up')
-            // console.log('if?')
-        }else{// 아래로 스크롤된다는 뜻
-            $('header').removeClass('up')
-            // console.log('else?')
-        }
-        if(scrolling > 0){ //스크롤내림
+        scrolling = $(window).scrollTop() //현재 스크롤값
+        if(scrolling > 0){
             $('header').addClass('fixed')
-        }else{ //0이거나 0보다 작은경우 (fixed제거)
+        }else{
             $('header').removeClass('fixed')
         }
     }
-    scroll_chk() //문서가 로딩되고 단 1번 실행
+
+    scroll_chk()//문서가 로딩되고 단 1번 실행
     $(window).scroll(function(){
-        scroll_chk() //스크롤 할때마다 실행
+        scroll_chk()//스크롤 될때마다 1번씩 실행
     })
-     /*----------- 끝 : 스크롤 시 header에 fixed -----------*/
+    /*----------- 끝 : 스크롤 시 header에 fixed  -----------*/
+
+   
 
 
 
 
-})//나가지마세욤
+})//맨끝 나가기금지
